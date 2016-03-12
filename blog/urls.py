30d6dd41907abf1eb98419adf06dc88955a80222
Chad Elder <chud92@msn.com>
2016-03-12 14:26:21 -0500

    2

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,6 @@ from blog import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^add_post/', views.add_post, name='add_post'),
+    url(r'^(?P<slug>[\w|\-]+)/$', views.post, name = 'post'),
 ]
--- bloggy_project/settings.py
+++ bloggy_project/settings.py
@@ -103,3 +103,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
